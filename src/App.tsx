4b20c7541abbb640c1635ab57231b81af3cda910
Vinícius <viniciusstorch62@gmail.com>
2026-05-11 16:30:08 -0300

    feat(core): setup react-toastify infrastructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { ToastContainer } from "react-toastify/unstyled";
 import AppRoutes from "./app/routes/AppRoutes";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        theme="colored"
+      />
+    </>
+  );
 }
 
 export default App;
