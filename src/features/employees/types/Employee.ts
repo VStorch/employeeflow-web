9d8fd7b2b73985fc998cbb173a40e355a98dfedd
Vinícius <viniciusstorch62@gmail.com>
2026-05-11 11:09:19 -0300

    refactor(employee): include missing IDs for Employee associations

--- src/features/employees/types/Employee.ts
+++ src/features/employees/types/Employee.ts
@@ -2,7 +2,12 @@ export type Employee = {
   id: number;
   name: string;
   email: string;
+
+  departmentId: number;
   department: string;
+
+  roleId: number;
   role: string;
+
   companyId: number;
 };
