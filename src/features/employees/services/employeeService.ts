bc51abcc5f8dea51a7dc2771383393d97db94233
Vinícius <viniciusstorch62@gmail.com>
2026-05-11 10:59:37 -0300

    feat(employee): complete CRUD methods in employee service

--- src/features/employees/services/employeeService.ts
+++ src/features/employees/services/employeeService.ts
@@ -4,11 +4,34 @@ import type { EmployeeFormData } from "../types/EmployeeFormData";
 
 export async function createEmployee(
   data: EmployeeFormData
-): Promise<void> {
-  await api.post("/employees", data);
+): Promise<Employee> {
+  const response = await api.post("/employees", data);
+  return response.data;
 }
 
-export async function getEmployees(): Promise<Employee[]> {
-    const response = await api.get("/employees");
-    return response.data;
+export async function getEmployees(
+  departmentId?: number,
+  roleId?: number
+): Promise<Employee[]> {
+  const response = await api.get<Employee[]>("/employees", {
+    params: { departmentId, roleId },
+  });
+  return response.data;
+}
+
+export async function getEmployeeById(id: number): Promise<Employee> {
+  const response = await api.get<Employee>(`/employees/${id}`);
+  return response.data;
+}
+
+export async function updateEmployee(
+  id: number,
+  data: EmployeeFormData
+): Promise<Employee> {
+  const response = await api.put<Employee>(`/employees/${id}`, data);
+  return response.data;
+}
+
+export async function deleteEmployee(id: number): Promise<void> {
+  await api.delete<void>(`/employees/${id}`);
 }
